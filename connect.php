969888028d025e93fcbbdafa1748a501cfda41e3
Hasnain Rafiq <tohusnain@hotmail.com>
2020-04-23 16:44:12 +0200

    connect file

--- connect.php
+++ connect.php
@@ -1,5 +1,12 @@
 <?php
-$conn = mysqli_connect("localhost", "mobidoc4_user", "r8zEVuwag3mAXA8AYAjAtIfO3adifE", "mobidoc1");
+/*manage db connectivity according to domain*/
+if($_SERVER['SERVER_NAME'] == 'w4.mobidoc.it')
+	$conn = mysqli_connect("79.137.84.125", "root", "duqAF8l5PoSE", "mobidoc1");
+else if($_SERVER['SERVER_NAME'] == 'localhost')
+	$conn = mysqli_connect("localhost", "root", "", "mobidoc1");
+else
+	$conn = mysqli_connect("localhost", "mobidoc4_user", "r8zEVuwag3mAXA8AYAjAtIfO3adifE", "mobidoc1");
+
 //	$conn = mysqli_connect("localhost", "mobidoc4_user", "8ag2Ju8I2A2aC8NaL4c7JEF8q8qiHI", "mobidoc1");
 //$conn = mysqli_connect("localhost", "root", "", "mobidoc1");
 //$conn = mysqli_connect("localhost", "mobidoc4_user", "1IToYIqOxasOlu28faNiXA4ETagEmA", "mobidoc1");
