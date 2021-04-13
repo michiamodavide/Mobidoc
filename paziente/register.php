94c4b6ba4413fa2689a51d0404facf9dbc2a0f1d
dw <dw@devrevival.com>
2021-04-13 16:16:29 +0500

    some change123

--- paziente/register.php
+++ paziente/register.php
@@ -1,29 +1,6 @@
 <!DOCTYPE html>
 <?php
 
-/*manage db connectivity according to domain*/
-if($_SERVER['SERVER_NAME'] == 'w4.mobidoc.it')
-  $conn = mysqli_connect("79.137.84.125", "root", "duqAF8l5PoSE", "mobidoc5");
-else if($_SERVER['SERVER_NAME'] == 'localhost')
-  $conn = mysqli_connect("localhost", "root", "", "mobidoc5");
-else
-  $conn = mysqli_connect("localhost", "mobidoc4_user", "r8zEVuwag3mAXA8AYAjAtIfO3adifE", "mobidoc5");
-
-//	$conn = mysqli_connect("localhost", "mobidoc4_user", "8ag2Ju8I2A2aC8NaL4c7JEF8q8qiHI", "mobidoc1");
-//$conn = mysqli_connect("localhost", "root", "", "mobidoc1");
-//$conn = mysqli_connect("localhost", "mobidoc4_user", "1IToYIqOxasOlu28faNiXA4ETagEmA", "mobidoc1");
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}else{
-  echo "Connected successfully";
-}
-
-$conn->query('SET NAMES utf8');
-
-
-exit();
-
  session_start();
  
  ?>
