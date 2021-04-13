acedff35504c71439d217d577f39c2e1e305df37
dw <dw@devrevival.com>
2021-04-13 19:51:44 +0500

    mobi changes done

--- professionisti/confirm_doc_visit.php
+++ professionisti/confirm_doc_visit.php
@@ -12,8 +12,30 @@ if($conn === false){
     $result = mysqli_query($conn, $sql);
    if ($result == 1) {
      // sql to delete a record
-     $sql22 = "DELETE FROM bookings where patient_id = '".$q."' and admin_book = 1";
-     $result22 = mysqli_query($conn, $sql22);
+
+     $sql222 = "select * from bookings where patient_id = '".$q."' and admin_book = 1";
+     $result222 = mysqli_query($conn, $sql222);
+
+     while ($rows222 = mysqli_fetch_array($result222)) {
+       $sql22 = "DELETE FROM bookings where booking_id = '".$rows222['booking_id']."'";
+       $result22 = mysqli_query($conn, $sql22);
+
+       if ($result22 == 1){
+
+         $sql223 = "DELETE FROM booked_service where booking_id = '".$rows222['booking_id']."'";
+         $result223 = mysqli_query($conn, $sql223);
+
+         if ($result223 ==1){
+//           not show anything
+         }else{
+           echo 'Record1 not deleted';
+         }
+
+       }else{
+         echo 'Record not deleted';
+       }
+     }
+
     }else{
      echo 'Record not Update';
    }
