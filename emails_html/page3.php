ba0471fabf9e9810d5875ce4c5568d8695c5b1d7
Danyal Waris <daniminhas75@gmail.com>
2021-06-08 14:30:29 +0500

    email testing

---
+++ emails_html/page3.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style>@charset "utf-8";body{font-family:Roboto;margin:0;padding:0;color:#000}p{color:#000;font-size:16px;line-height:30px;font-weight:300}a{transition:.3s linear;text-decoration:none}a:focus,a:hover{text-decoration:none}li,ul{list-style:none;margin:0;padding:0}h1{margin-top:0}.main-wraper{width:50%;margin:0 auto}.wraper{width:100%;display:block;clear:both;height:60px}.left-div{width:15%;float:left;padding-top:15px;font-size:16px;padding-right:3px}.right-div{width:84%;float:left}.form1{margin:50px 0;font-size:14px}.form1 .form-control{display:block;width:100%;height:auto;padding:6px 12px;font-size:14px;line-height:1.42857143;color:#555;background-color:#fff;background-image:none;border:none;border-bottom:1px solid #000;border-radius:0;-webkit-box-shadow:none;box-shadow:none;-webkit-transition:border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;-o-transition:border-color ease-in-out .15s,box-shadow ease-in-out .15s;transition:border-color ease-in-out .15s,box-shadow ease-in-out .15s}input:focus-visible{outline:0!important;box-sizing:none!important}p{font-size:16px;line-height:26px}p a{font-size:16px;line-height:26px;font-weight:700;color:#4472c4;text-decoration:underline}p a:hover{color:red}h1{font-size:22px;line-height:26px;text-align:center;font-weight:700;margin-bottom:20px}h2{font-size:18px;line-height:26px;font-weight:700}h3{font-size:18px;line-height:26px;font-weight:700;color:red;font-style:italic}h4{font-size:20px;line-height:26px;font-weight:700;color:red;font-style:italic;margin:50px 0}ul li{font-size:16px;line-height:26px;padding-left:20px;margin-left:15px}ul.dot li{font-size:16px;line-height:26px;list-style:disc;padding-left:20px;margin:20px 0 20px 15px}ol li{font-size:12px;line-height:22px;list-style:decimal;padding-left:20px;margin:20px 0 20px 15px}.line{border-bottom:1px solid #000;width:250px;display:block}ul.abc li{font-size:16px;line-height:22px;list-style:lower-alpha;padding-left:10px;margin:20px 0 20px 60px}ul.arrow{position:relative;list-style:none}ul.arrow li{margin-bottom:20px}ul.arrow li::before{content:'⮚';position:absolute;left:0;font-size:26px}ul.center-list{text-align:center;margin:50px 0}ul.center-list li{list-style:none;display:inline-block;margin:0 40px;font-weight:700}.page-number{font-size:18px;line-height:26px;text-align:center;font-weight:700;margin:40px 0;padding:30px 0 10px 0}@media only screen and (max-width:1600px){.right-div{width:75%}.left-div{width:17%}}@media only screen and (max-width:1536px){.left-div{width:18%}}@media only screen and (max-width:1440px){.left-div{width:19%}}@media only screen and (max-width:1367px){.left-div{width:20%}}@media only screen and (max-width:1300px){.left-div{width:21%}}@media only screen and (max-width:1024px){.right-div{width:60%}.left-div{width:35%}}@media only screen and (max-width:767px){.right-div{width:50%}.left-div{width:48%}}</style>
+</head>
+<body>
+<div class="main-wraper">
+    <p>nazionale ed europea, con l’adozione, previo adeguato processo di valutazione del rischio, delle misure di protezione – tecniche, organizzative e procedurali – allo stato più idonee ad evitare la perdita accidentale, la distruzione fortuita, l’uso illecito o non corretto, l’accesso non autorizzato.<br>
+        <br>
+        I Suoi dati personali saranno conservati dalla nostra Società, ai fini di una migliore gestione del servizio erogato agli interessati, per 2 (due) anni dall’ultima corrispondenza, salvo: </p>
+    <ul class="dot">
+        <li>precedente intervento della cessazione della finalità di trattamento;</li>
+        <li>precedente richiesta di cancellazione dell’interessato;</li>
+        <li>gli obblighi di conservazione previsti dalla legge, anche con specifico riguardo alle prestazioni sanitarie erogate<sup>6</sup>;</li>
+        <li>sussista la necessità che siano trattati dalla nostra Società per esercitare o difendere un diritto in sede giudiziaria.</li>
+    </ul>
+    <br>
+    <h2>5. Comunicazione e diffusione dei dati personali</h2>
+    <br>
+    <p>Il paragrafo che segue illustra se ed in che modo saranno trasferiti i Suoi dati personali.<br>
+        <br>
+        I Suoi dati personali non saranno diffusi, né trasferiti verso Paesi terzi o organizzazioni internazionali<sup>7</sup>.<br>
+        <br>
+        I Suoi dati personali potranno essere comunicati esclusivamente a:<br>
+        <br>
+        in relazione alla finalità prevista alla lettera a) del terzo paragrafo della presente informativa: </p>
+    <ul class="abc">
+        <li>consulenti della società incaricati degli adempimenti previsti dalla Legge o dal C.C.N.L. applicabile;</li>
+        <li>società che svolgono servizi bancari, finanziari e assicurativi;</li>
+        <li>società che svolgono attività di trasmissione, imbustamento, trasporto e smistamento di comunicazioni ai dipendenti;</li>
+        <li>società che svolgono servizi di archiviazione della documentazione relativa ai rapporti intercorsi con i Clienti;</li>
+    </ul>
+    <p>e inoltre, in relazione alla finalità prevista alla lettera b) del terzo paragrafo della presente informativa, esclusivamente a soggetti terzi ai quali la comunicazione si renda necessaria per perseguirla tecnicamente<sup>8</sup>.</p>
+    <h2>6.  Diritti dell’interessato</h2>
+    <div class="line"></div>
+    <ol start="6">
+        <li>A titolo esemplificativo e non esaustivo: la documentazione inerente gli accertamenti effettuati nel corso delle visite per il rilascio del certificato di idoneità all’attività sportiva agonistica deve essere conservata, a cura del medico visitatore, per almeno 5 (cinque) anni (ai sensi dell’art. 5, D.M. 18/2/1982); le cartelle cliniche, unitamente ai relativi referti, vanno conservate illimitatamente (in base alla Circolare del Ministero della Sanità del 19/12/1986, n. 9002/AG454/260); la documentazione iconografica radiologica deve essere conservata per un periodo non inferiore a 10 (dieci) anni (ai sensi dell’art. 4, D.M. 14/2/1997).</li>
+        <li>In particolare, in presenza delle previste dal Capo V del Regolamento U.E. n. 679/2016.</li>
+        <li>Con modalità che comprendono, a titolo esemplificativo, l'invio di comunicazioni di carattere commerciale, informativo e promo-pubblicitario su prodotti, servizi ed attività di Tekamed S.r.l.</li>
+    </ol>
+    <div class="page-number">3</div>
+</div>
+</body>
+</html>
