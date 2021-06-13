6efda634ac9832dd0dfe25128c4e1ac6fb47a727
hasnain <daniminhas75@gmail.com>
2021-06-13 18:10:17 +0500

    emails fixed

--- booking-submit.php
+++ booking-submit.php
@@ -155,18 +155,18 @@ WHERE lis.article_mobidoc_id='".$article_id."' AND lis.doctor_id='".$doctor_id."
 
                     $send_emails_array = array($contact_email, $doctor_email, "jimmymike347@gmail.com"); //info@mobidoc.it
 
+                    $contact_full_n = $contact_name;
+                    include ("contact_pdf.php");
+                    // Attachment file
+                    $pdf_file1 = "assets/generate_pdf/".strtolower($contact_fname).'.pdf';
+
+                    include ("executor_pdf.php");
+                    // Attachment file
+                    $pdf_file2 = "assets/generate_pdf/".strtolower($doctor_fname).'.pdf';
+
+                    $pdf_files = array($pdf_file1, $pdf_file2);
+
                     foreach($send_emails_array as $send_emails_key => $send_email) {
-
-                        $contact_full_n = $contact_name;
-                        include ("contact_pdf.php");
-                        // Attachment file
-                        $pdf_file1 = "assets/generate_pdf/".strtolower($contact_fname).'.pdf';
-
-                        include ("executor_pdf.php");
-                        // Attachment file
-                        $pdf_file2 = "assets/generate_pdf/".strtolower($contact_fname).'.pdf';
-
-                        $pdf_files = array($pdf_file1, $pdf_file2);
 
                         // Recipient
                         $to = $send_email;
@@ -175,7 +175,15 @@ WHERE lis.article_mobidoc_id='".$article_id."' AND lis.doctor_id='".$doctor_id."
                         $from = 'mobidoc_update@mobidoc.it';
                         $rply_email = 'noreplay@mobidoc.it';
 
-                        // Email body content
+                        // Header for sender info
+                        $headers .= 'From: '.$from."\r\n". 'Reply-To: '.$rply_email."\r\n" .   'X-Mailer: PHP/' . phpversion();
+
+                        // Boundary
+                        $semi_rand = md5(time());
+                        $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
+
+                        // Headers for attachment
+                        $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";
 
                         // Compose a simple HTML email message
                         $htmlContent = '<!DOCTYPE html><html data-wf-page="5dcd852d5095d024f8ea51ae" data-wf-site="5dcd852d5095d04927ea51ad"><head><meta charset="utf-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta content="Webflow" name="generator"><style>.header{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;width:100%;height:180px;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;background-image:url("https://www.mobidoc.it/images/mailer_header.png");background-position:50% 50%, 50% 0%;background-size:100%,cover;background-repeat:no-repeat,repeat}.text_container{width:80%;min-height:150px;margin-top:70px;margin-right:auto;margin-left:auto}.button{margin:20px 20px 20px 0px;padding:16px 34px;border-radius:50px;background-color:#00285c}.text-block{margin-top:10px;font-size:16px}.text-block.italic{margin-top:28px;font-style:italic;font-weight:300}.body{font-family:Poppins,sans-serif;color:#00285c}.heading{margin-bottom:23px}.name{width:auto}a{text-decoration:none;color:#fff}</style> <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script> <script type="text/javascript">WebFont.load({google:{families:["Poppins:300,regular,italic,600"]}});</script> <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script> </head><body class="body"><div class="header"></div><div class="text_container"><h4 class="heading">Nuova Prenotazione!</h4><div class="text-block">'.$paziente_main_name.' Ha prenotato una visita da '.$doctor_main_name.'. <br><br><strong>Contact Info</strong>:
@@ -193,16 +201,6 @@ WHERE lis.article_mobidoc_id='".$article_id."' AND lis.doctor_id='".$doctor_id."
                         }
                         $htmlContent .="<br><strong>Doctor Info<br>Name</strong>: ".$doctor_main_name."<br><strong>Email</strong>: ".$doctor_email."<br><strong>Phone</strong>: ".$doctor_phone."<br><br><strong>Data e Ora</strong>:da confermare<br><strong>Payment Method: </strong>".$payment_mode." <br><br>Questa email è stata generata da un sistema automatico, si prega di non rispondere.<br><br> Cordiali Saluti,<br> La Direzione Mobidoc</div> <br></div></body></html>";
 
-
-                        // Header for sender info
-                        $headers .= 'From: '.$from."\r\n". 'Reply-To: '.$rply_email."\r\n" .   'X-Mailer: PHP/' . phpversion();
-
-                        // Boundary
-                        $semi_rand = md5(time());
-                        $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
-
-                        // Headers for attachment
-                        $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";
 
                         // Multipart boundary
                         $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
@@ -251,15 +249,14 @@ WHERE lis.article_mobidoc_id='".$article_id."' AND lis.doctor_id='".$doctor_id."
                         // Send email
                         @mail($to, $subject, $message, $headers, $returnpath);
 
-                           for($pd=0;$pd<count($pdf_files);$pd++){
-                               unlink($pdf_files[$pd]);
-                           }
-
                         // Email sending status
                        // echo $mail?"<h1>Email Sent Successfully!</h1>":"<h1>Email sending failed.</h1>";
                     }
 
 
+                    for($pd=0;$pd < count($pdf_files);$pd++){
+                        unlink($pdf_files[$pd]);
+                    }
                     /*
 
                     //email to admin
