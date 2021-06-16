2acbb16e354df9ca3a5998e6c2163dc8ea15950d
hasnain <daniminhas75@gmail.com>
2021-06-16 23:08:48 +0500

    changes done

--- paziente/profile_edit_php.php
+++ paziente/profile_edit_php.php
@@ -1,34 +1,36 @@
-<?php 	session_start();
-		$_SESSION['paziente_email']=$_POST['email'];
-		include '../connect.php';
-		$email = $_SESSION['paziente_email'];
-   $fname = mysqli_real_escape_string($conn, $_POST['first_name']);
-		$lname = mysqli_real_escape_string($conn, $_POST['last_name']);
-		$tel = mysqli_real_escape_string($conn, $_POST['tele']);
-		$check = mysqli_real_escape_string($conn, $_POST['privacy_check']);
+<?php session_start();
+$_SESSION['paziente_email'] = $_POST['email'];
+include '../connect.php';
 
-		$sql = "update contact_profile set name='".$fname."', surname='".$lname."', phone='".$tel."', privacy_consent='".$check."' where email='".$email."'";
+$email = $_SESSION['paziente_email'];
+$fname = mysqli_real_escape_string($conn, $_POST['first_name']);
+$lname = mysqli_real_escape_string($conn, $_POST['last_name']);
+$tel = mysqli_real_escape_string($conn, $_POST['tele']);
 
-		$result = mysqli_query($conn, $sql);
-		if($result==1)
-			echo "Record inserted1";
-		else{
-			echo "Unable to insert record1"; 
-			mysqli_close($conn);
-		}
-		
-		/*$sql3 = "update doctor_register set tick = 1 where email = '".$email."' ";
-		$result = mysqli_query($conn, $sql3);*/
+/*
+$marketing_consent = 'N';
+if (isset($_POST['marketing_consent']) && $_POST['marketing_consent'] == 'Y') {
+    $marketing_consent = 'Y';
+}
+*/
 
-		mysqli_close($conn);
+date_default_timezone_set("Europe/Rome");
+$marketing_consent_date = date("Y/m/d H:i:s");
 
-		if(isset($_COOKIE["Booking_name"], $_COOKIE["Booking_id"], $_COOKIE["article_id"]) ){
-			$booking_name = $_COOKIE["Booking_name"];		
-			$doctor_id = $_COOKIE["Booking_id"];
-      $article_id = $_COOKIE["article_id"];
-			$link = "/checkout.php?book_visit=".$booking_name."&book_doctor=".$doctor_id."&article_id=".$article_id;
-			header('Location: '.$link);
-		} else {
-			header("Location: account.php");
-		}
+$sql = "update contact_profile set name='" . $fname . "', surname='" . $lname . "', phone='" . $tel . "' where email='" . $email . "'";
+
+$result = mysqli_query($conn, $sql);
+if ($result == 1)
+    echo "Record inserted1";
+else {
+    echo "Unable to insert record1";
+    mysqli_close($conn);
+}
+
+/*$sql3 = "update doctor_register set tick = 1 where email = '".$email."' ";
+$result = mysqli_query($conn, $sql3);*/
+
+mysqli_close($conn);
+
+header("Location: account.php");
 ?>
