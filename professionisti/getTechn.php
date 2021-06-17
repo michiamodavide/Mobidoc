6c9b254b6843c2ad29ac2ed2ba33710471659d50
hasnain <daniminhas75@gmail.com>
2021-06-17 16:04:26 +0500

    some chagnes done

--- professionisti/getTechn.php
+++ professionisti/getTechn.php
@@ -22,38 +22,50 @@ JOIN listini ls ON ds.doctor_id=ls.doctor_id
  WHERE ds.specialty = '".$doc_spec_row['specialty']."' AND ls.article_mobidoc_id='".$article_id."' AND dp.`active`='Y' AND dp.`visible`='Y' AND dp.`puo_refertare`='Y' AND dg.tick = 1";
 
 $result = mysqli_query($conn, $sql);
+$result_count = mysqli_num_rows($result);
 
-while($rows2 = mysqli_fetch_array($result)){
+if ($result_count > 0) {
+    while ($rows2 = mysqli_fetch_array($result)) {
 //  $doctor_id = $rows2['doctor_id'];
 //  $sql2 = "select * from doctor_profile where doctor_id ='".$doctor_id."' AND puo_refertare ='Y' ";
 //  $result2 = mysqli_query($conn, $sql2);
 //  $rows2 = mysqli_fetch_array($result2);
-    $profile_image = "/professionisti/".$rows2['photo'];
-    $name = $rows2['fname']." ".$rows2['lname'];
-    $titile = ucwords($rows2['title']);
-    $link = "/il-team/professionista.php?".$rows2['doctor_id'];
-    $id = $rows2['doctor_id'];
-         if ($rows2['email'] !== $_SESSION['doctor_email']){
-     ?>
+        $profile_image = "/professionisti/" . $rows2['photo'];
+        $name = $rows2['fname'] . " " . $rows2['lname'];
+        $titile = ucwords($rows2['title']);
+        $link = "/il-team/professionista.php?" . $rows2['doctor_id'];
+        $id = $rows2['doctor_id'];
+        if ($rows2['email'] !== $_SESSION['doctor_email']) {
+            ?>
 
-     <div class="professionist_card-2 selecting">
-      <div class="professionist_image_container">
-       <img src="<?php echo $profile_image; ?>" alt="" class="professionist_image">
-       <div class="selected_tick">
-        <img src="../images/Path-107.svg" width="55" alt="" class="image-4">
-       </div>
-      </div>
-      <div class="preofessionist_name"><?php echo $name; ?></div>
-      <div class="professionist_title"><?php echo $titile; ?></div>
-      <div class="button_container">
-       <a href="#" class="button-3 gradient select_button w-button" doctor-id="<?php echo $id; ?>" excuter-email="<?php echo $rows2['email']; ?>" executer-name="<?php echo $name; ?>"
-          onClick="setDoctor(this.getAttribute('doctor-id'),this.getAttribute('excuter-email'),this.getAttribute('executer-name')); $('.selected_tick').removeClass('selected_true'); $(this).parent().siblings('.professionist_image_container').children('.selected_tick').toggleClass('selected_true');">Seleziona</a>
-       <a href="<?php echo $link; ?>" class="button-3 w-button" target="_blank">Esplora profilo</a>
-      </div>
-     </div>
+            <div class="professionist_card-2 selecting">
+                <div class="professionist_image_container">
+                    <img src="<?php echo $profile_image; ?>" alt="" class="professionist_image">
+                    <div class="selected_tick">
+                        <img src="../images/Path-107.svg" width="55" alt="" class="image-4">
+                    </div>
+                </div>
+                <div class="preofessionist_name"><?php echo $name; ?></div>
+                <div class="professionist_title"><?php echo $titile; ?></div>
+                <div class="button_container">
+                    <a href="#" class="button-3 gradient select_button w-button" doctor-id="<?php echo $id; ?>"
+                       excuter-email="<?php echo $rows2['email']; ?>" executer-name="<?php echo $name; ?>"
+                       onClick="setDoctor(this.getAttribute('doctor-id'),this.getAttribute('excuter-email'),this.getAttribute('executer-name')); $('.selected_tick').removeClass('selected_true'); $(this).parent().siblings('.professionist_image_container').children('.selected_tick').toggleClass('selected_true');">Seleziona</a>
+                    <a href="<?php echo $link; ?>" class="button-3 w-button" target="_blank">Esplora profilo</a>
+                </div>
+            </div>
 
-      <?php
+            <?php
+        }
     }
-} mysqli_close($conn);
+    mysqli_close($conn);
 
+}else{
 ?>
+    <p id="no_refer_text" style="text-align: center;color: red;">No referrer available for the selected visit.</p>
+    <style>
+        #ext_not{
+          display: none;
+        }
+    </style>
+<?php }?>
