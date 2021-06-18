18d81234fb76035f353981dd96bf3f33b867048b
hasnain <daniminhas75@gmail.com>
2021-06-18 18:59:11 +0500

    emails done

--- admin/patient_reg.php
+++ admin/patient_reg.php
@@ -320,7 +320,7 @@ if(isset($_POST['submit'])){
              $pdf_files = array($pdf_file1, $pdf_file2, $pdf_file3);
 
 
-             $send_emails_array = array($contact_email, $doctor_email, "jimmymike347@gmail.com"); //info@mobidoc.it
+             $send_emails_array = array($contact_email, $doctor_email, "info@mobidoc.it"); //info@mobidoc.it
 
              $subject = 'Nuova Prenotazione!';
              // Sender
@@ -524,11 +524,13 @@ if(isset($_POST['submit'])){
                  $htmlContent1 = '<!DOCTYPE html><html data-wf-page="5dcd852d5095d024f8ea51ae" data-wf-site="5dcd852d5095d04927ea51ad"><head><meta charset="utf-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta content="Webflow" name="generator"><style>.header{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;width:100%;height:180px;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;background-image:url("https://www.mobidoc.it/images/mailer_header.png");background-position:50% 50%, 50% 0%;background-size:100%,cover;background-repeat:no-repeat,repeat}.text_container{width:80%;min-height:150px;margin-top:70px;margin-right:auto;margin-left:auto}.button{margin:20px 20px 20px 0px;padding:16px 34px;border-radius:50px;background-color:#00285c}.text-block{margin-top:10px;font-size:16px}.text-block.italic{margin-top:28px;font-style:italic;font-weight:300}.body{font-family:Poppins,sans-serif;color:#00285c}.heading{margin-bottom:23px}.name{width:auto}a{text-decoration:none;color:#fff}</style> <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script> <script type="text/javascript">WebFont.load({google:{families:["Poppins:300,regular,italic,600"]}});</script> <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script> </head><body class="body"><div class="header"></div><div class="text_container"><h4 class="heading">Nuova Prenotazione in attesa dell approvazione dell esecutore!</h4><div class="text-block">Le comunichiamo che le è stata assegnata una prenotazione:<br><br><strong>Contact Info</strong>:
               <br><strong>Name</strong>: '.$contact_name.'<br><strong>Phone</strong>: '.$contact_phone.'<br><br><strong>Patient Info</strong><br><strong>Name</strong>: '.$patient_name.'<br><strong>Fiscal Code</strong>: '.$patient_fiscal.'<br><strong>Date of Birth</strong>: '.$patient_dob.'<br><strong>Address</strong>: <a target=\'_blank\' style=\'color: blue; text-decoration: underline\' href='.$patient_gmap_addr.'>'.$patient_address.'</a><br><br><strong>Visits/Exams</strong><br>';
 
+                 echo $doc_ids_array[$ij];
                  foreach($visit_data[$doc_ids_array[$ij]] as $vis_key => $vis_data) {
                      $visit_amount = $vis_data['exam_price'].'</strong>';
                      if ($vis_key > 0 && !empty($discounted_value[$vis_key-1])){
                          $is_discount =$discounted_value[$vis_key-1];
-                         $disounted_amount = $vis_data['exam_price']*$is_discount/100;
+                         $dis_amount = $vis_data['exam_price']*$is_discount/100;
+                         $disounted_amount = $vis_data['exam_price']-$dis_amount;
                          $visit_amount = $disounted_amount.' </strong>(With '.$is_discount.'% discount)';
                      }
                      $htmlContent1 .="
@@ -536,15 +538,12 @@ if(isset($_POST['submit'])){
 ";
                  }
                  $htmlContent1 .="<br>Clicca il seguente link per connetterti alla tua dashboard e visualizzare e/o modificare i dettagli della prenotazione: <a target='_blank' style='color: blue; text-decoration: underline' href='https://mobidoc.it/professionisti/account.php'>https://mobidoc.it/professionisti/account.php</a></div> <br></div></body></html>";
-                 echo $htmlContent1;
-                 //echo $htmlContent1;
                  mail($email_to, $subject2, $htmlContent1, $headers2);
 
              }
 
-
              /*admin email*/
-             $admin_email = 'jimmymike347@gmail.com';
+             $admin_email = 'info@mobidoc.it';
              $visit_data = array();
              $doctor_names = array();
              $select_reporter_array = array();
