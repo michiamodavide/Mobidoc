98b93c1b2e253d13d3b405ad2cbb61aacb0b081c
hasnain <daniminhas75@gmail.com>
2021-06-21 17:11:31 +0500

    changes done

--- admin/patient_reg.php
+++ admin/patient_reg.php
@@ -144,10 +144,7 @@ if(isset($_POST['submit'])){
       date_default_timezone_set("Europe/Rome");
       $date_of_booking = date("Y/m/d H:i:s");
 
-        $payment_mode = $_POST['payment_mode'];
-        if ($_POST['payment_mode'] == 'online'){
-          $payment_mode = 'Carta di Credito';
-      }
+      $payment_mode = $_POST['payment_mode'];
 
       $km_price = $_POST['km_price'];
       $booking_status = 1;
@@ -433,7 +430,7 @@ if(isset($_POST['submit'])){
 
 
              $paypal_link = '';
-             if ($_POST['payment_mode'] == "online"){
+                 if ($payment_mode == "Carta di Credito"){
                  include ("../pay/redirect_tele.php");
                  $paypal_link = " | <a target='_blank' style='color: blue; text-decoration: underline' href='$redirectUrl'>Clicca qui per pagare</a>";
              }
@@ -551,7 +548,7 @@ if(isset($_POST['submit'])){
                  $htmlContent1 = '<!DOCTYPE html><html data-wf-page="5dcd852d5095d024f8ea51ae" data-wf-site="5dcd852d5095d04927ea51ad"><head><meta charset="utf-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta content="Webflow" name="generator"><style>.header{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;width:100%;height:180px;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;background-image:url("https://www.mobidoc.it/images/mailer_header.png");background-position:50% 50%, 50% 0%;background-size:100%,cover;background-repeat:no-repeat,repeat}.text_container{width:80%;min-height:150px;margin-top:70px;margin-right:auto;margin-left:auto}.button{margin:20px 20px 20px 0px;padding:16px 34px;border-radius:50px;background-color:#00285c}.text-block{margin-top:10px;font-size:16px}.text-block.italic{margin-top:28px;font-style:italic;font-weight:300}.body{font-family:Poppins,sans-serif;color:#00285c}.heading{margin-bottom:23px}.name{width:auto}a{text-decoration:none;color:#fff}</style> <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script> <script type="text/javascript">WebFont.load({google:{families:["Poppins:300,regular,italic,600"]}});</script> <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script> </head><body class="body"><div class="header"></div><div class="text_container"><h4 class="heading">Nuova Prenotazione in attesa dell approvazione dell esecutore!</h4><div class="text-block">Le comunichiamo che le è stata assegnata una prenotazione:<br><br><strong>Contact Info</strong>:
               <br><strong>Name</strong>: '.$contact_name.'<br><strong>Phone</strong>: '.$contact_phone.'<br><br><strong>Patient Info</strong><br><strong>Name</strong>: '.$patient_name.'<br><strong>Fiscal Code</strong>: '.$patient_fiscal.'<br><strong>Date of Birth</strong>: '.$patient_dob.'<br><strong>Address</strong>: <a target=\'_blank\' style=\'color: blue; text-decoration: underline\' href='.$patient_gmap_addr.'>'.$patient_address.'</a><br><br><strong>Visits/Exams</strong><br>';
 
-                 echo $doc_ids_array[$ij];
+                 //echo $doc_ids_array[$ij];
                  foreach($visit_data[$doc_ids_array[$ij]] as $vis_key => $vis_data) {
                      $visit_amount = $vis_data['exam_price'].'</strong>';
                      if ($vis_key > 0 && !empty($discounted_value[$vis_key-1])){
